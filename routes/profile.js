2b574b0fb1dc8784dc4355feef78d3269333555c
Heidi Sokolovski <heidi.sokolovski@gmail.com>
2024-01-13 16:23:30 +0100

    user data displayed

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', requiresAuth(), async (req, res, next) => {
-    res.render('profile', {});
+    const user = await req.oidc.fetchUserInfo();
+    res.render('profile', {user});
 });
 
 module.exports = router;
